407684fbd1235d41f87b6d5373a1e601e0e1eb77
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-05 14:29:22 +0530

    creating class student

---
+++ Student.java
@@ -0,0 +1,6 @@
+class Student {
+    private String name; // Student's name
+    private long prn; // Student's PRN 
+    private int age; // Student's age
+    private double cgpa; // Student's CGPA 
+    private LocalDate dob; // Student's Date of Birth
