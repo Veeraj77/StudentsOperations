f52039932d0af95abd12ae100d26e1c1ee57fde1
Veeraj77 <Veerajsahu07@gmail.com>
2025-03-05 14:25:30 +0530

    while loop

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
 class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations();
+
+        while (true) { // Menu loop
+            System.out.println("\nMenu:");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Edit Student");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = Integer.parseInt(scan.nextLine());
